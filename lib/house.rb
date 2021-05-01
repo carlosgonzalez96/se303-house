2da1642f40b4f984a06debb0f89a3d8401f4d5ce
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-04-30 20:25:27 -0700

    Add sixth line of the verse lyric

--- lib/house.rb
+++ lib/house.rb
@@ -11,6 +11,7 @@ class House
         elsif(lineNumber == 5)
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         elsif(lineNumber == 6)
+            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
