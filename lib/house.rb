e95346a6f9541a9d06a7036dc3667b0bb98f737c
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-04-30 20:28:26 -0700

    Add seventh line of the verse lyric

--- lib/house.rb
+++ lib/house.rb
@@ -13,6 +13,7 @@ class House
         elsif(lineNumber == 6)
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         elsif(lineNumber == 7)
+            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
