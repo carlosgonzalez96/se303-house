2d4273c3b67f4d44f6ea65f0ec76cb403f95c4c0
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-04-30 20:31:08 -0700

    Add conditional if statement for ninth line of the verse

--- lib/house.rb
+++ lib/house.rb
@@ -16,6 +16,7 @@ class House
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         elsif(lineNumber == 8)
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        elsif(lineNumber == 9)
         end
     end
 end
