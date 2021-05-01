be9973382a7b6e249ac8b21397eaa4d349f95333
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-04-30 20:15:50 -0700

    Add conditional if statement for second line of the verse

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
     def line(lineNumber)
         if(lineNumber == 1)
             "This is the house that Jack built.\n"
+        elsif(lineNumber == 2)
+            "This is the malt that lay in the house that Jack built.\n"
         end
     end
 end
