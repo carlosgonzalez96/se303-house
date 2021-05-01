502812c06953446ead3f77897158e9ce491f9729
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-04-30 20:34:43 -0700

    Add conditional if statement for eleventh line of the verse

--- lib/house.rb
+++ lib/house.rb
@@ -20,6 +20,7 @@ class House
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         elsif(lineNumber == 10)
             "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        elsif(lineNumber == 11)
         end
     end
 end
