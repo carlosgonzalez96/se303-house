10b41849c882c65f72704eaef174de1be50b9b92
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-09 14:36:07 -0700

    changed if conditions to cases

--- lib/house.rb
+++ lib/house.rb
@@ -1,28 +1,29 @@
 class House
-    def line(lineNumber)
-        if(lineNumber == 1)
+    def line(nextLine)
+        case nextLine
+        when 1
             "This is the house that Jack built.\n"
-        elsif(lineNumber == 2)
+        when 2
             "This is the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 3)
+        when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 4)
+        when 4
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 5)
+        when 5
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 6)
+        when 6
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 7)
+        when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 8)
+        when 8
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 9)
+        when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 10)
+        when 10
             "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 11)
+        when 11
             "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif(lineNumber == 12)
+        when 12
             "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
