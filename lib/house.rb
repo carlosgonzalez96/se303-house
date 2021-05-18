257d67994e753ba7aab604fad3e940b0afd81387
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-18 12:05:14 -0700

    Back to shameless green

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
 
+    def phrase(number)
+        "the horse and the hound and the horn that belonged to"
+    end
+
     def line(number)
         case number
         when 1
@@ -25,7 +29,7 @@ class House
         when 11
             "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 12
-            "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)} the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 
