e21f0812ca9e30688c2fd6946968f61d1963ebf5
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-19 12:55:49 -0700

    call the phrase funciton when number is 2

--- lib/house.rb
+++ lib/house.rb
@@ -9,7 +9,7 @@ class House
         when 1
             "This is the house that Jack built.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)}the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
