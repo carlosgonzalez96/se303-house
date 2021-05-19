f88f4e0a1dbd891b9c1c2617feccd099d07be1d1
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-19 13:29:11 -0700

    achive sameness between line 8 and else

--- lib/house.rb
+++ lib/house.rb
@@ -14,7 +14,7 @@ class House
         elsif number == 7
             "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         elsif number == 8
-            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in"
+            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
             ""
         end
@@ -23,7 +23,7 @@ class House
     def line(number)
         case number
         when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)}the house that Jack built.\n"
         when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
