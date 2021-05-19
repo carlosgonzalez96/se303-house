2c9751703dae61d0ba7bc133f4c0df46ce1e8956
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-19 13:30:58 -0700

    address the next diffrence between the else clause and line 9

--- lib/house.rb
+++ lib/house.rb
@@ -15,6 +15,8 @@ class House
             "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         elsif number == 8
             "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        elsif number == 9
+            "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay "
         else
             ""
         end
