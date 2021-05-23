b08a98fd6da355e86876c92aa3e49365da54c57d
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-23 16:50:59 -0700

    add the different phrase between number 8 and 7 to the new array

--- lib/house.rb
+++ lib/house.rb
@@ -6,6 +6,7 @@ class House
             'the farmer sowing his corn that kept',
             'the rooster that crowed in the morn that woke',
             'the priest all shaven and shorn that married',
+            'the man all tattered and torn that kissed',
 
         ]
     end
