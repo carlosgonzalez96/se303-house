c561ae82b9ce097c2763b82b61ccafa7b232f270
carlosgonzalez96 <carlosglez817@gmail.com>
2021-05-30 18:15:53 -0700

    Initizalized variabes that holds array of the verbs and subjects

--- lib/house.rb
+++ lib/house.rb
@@ -41,5 +41,8 @@ class RandomHouse < House
 end
 
 class RandomVerseHouse < House
-
+    def initialize(prelude= "This is")
+        @verbs = ['that belonged to','that kept','that woke','that married','that kissed','that milked','that tossed','that worried','that killed','that ate','that lay in']
+        @subjects =['the horse and the hound and the horn','the farmer sowing his corn','the rooster that crowed in the morn','the priest all shaven and shorn','the man all tattered and torn','the maiden all forlorn','the cow with the crumpled horn','the dog','the cat','the rat','the malt']
+    end
 end
